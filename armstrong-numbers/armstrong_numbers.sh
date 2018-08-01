1cb3055a2cd4f9eb978355cfd1f133cb5a0cbfbc
David S. Jackson <deepbsd@yahoo.com>
2018-08-01 19:00:12 -0400

    finished scrabble_score, started armstrong_numbers

---
+++ armstrong-numbers/armstrong_numbers.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+input=${PS1}
+power=${#input}
+
+
+val=0
+
+for n in ${input[@]}; do
+    ((val+=n**${power}))
+done
+
+[[ ${val} -eq ${input} ]] && echo "true" && exit 0
+
+echo "false" && exit 1
