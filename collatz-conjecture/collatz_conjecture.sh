953abc61ad70a3f042fa283c617d84b17a14c6b5
David S. Jackson <deepbsd@yahoo.com>
2018-08-11 15:14:16 -0400

    modified the collatz conjecture solution

--- collatz-conjecture/collatz_conjecture.sh
+++ collatz-conjecture/collatz_conjecture.sh
@@ -19,12 +19,11 @@ count=0   # count iterations...
 
 # must validate input or loop is infinite
 while [[ ${posnum} -ne 1 ]]; do
-  if [ $((posnum % 2)) -eq 0 ]; then
-    ((posnum=posnum/2))
-  else 
-    ((posnum=1+posnum*3))
-  fi
+  [ $((posnum % 2)) -eq 0 ] && ((posnum=posnum/2)) || ((posnum=1+posnum*3))
   ((count+=1))
 done
 
 echo $count
+
+
+
