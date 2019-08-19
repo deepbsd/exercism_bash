f73e5768a382d624f4289dcfa2eafd74543c18c5
David S. Jackson <deepbsd@yahoo.com>
2019-08-18 23:13:21 -0400

    very close on beer song

--- beer-song/beer_song.sh
+++ beer-song/beer_song.sh
@@ -14,18 +14,24 @@ error(){
 say_verse(){
 
     bottle_num=$1
+    bottle_num1=$((bottle_num-1))
     bottle_sing="bottle"
     bottle_plural="bottles"
     verse2="Take one down and pass it around,"
     verse3="Go to the store and get some more,"
-    
-
-    if [[ $bottle_num -gt 1 ]]; then
-        bottle_num1=$((bottle_num-1)) 
+ 
+   if [[ $bottle_num -gt 2 ]]; then
+        #bottle_num1=$((bottle_num-1)) 
         bottles1=${bottle_plural}
         bottles2=${bottle_plural}
     fi
 
+    if [[ $bottle_num -eq 2 ]]; then
+        #bottle_num1=$((bottle_num-1)) 
+        bottles1=${bottle_plural}
+        bottles2=${bottle_sing}
+    fi
+ 
     if [[ $bottle_num -eq 1 ]]; then
         bottle_num1="no more"
         bottles1=${bottle_sing}
@@ -41,12 +47,20 @@ say_verse(){
     fi
 
 
-    verse="${bottle_num} ${bottles1} of beer on the wall, ${bottle_num} ${bottles1} of beer.  ${verse2} ${bottle_num1} ${bottles2} of beer on the wall." 
+    verse="${bottle_num} ${bottles1} of beer on the wall, ${bottle_num} ${bottles1} of beer. 
+${verse2} ${bottle_num1} ${bottles2} of beer on the wall." 
 
 
     echo ${verse} 
 }
 
-say_verse $input
+[[ ${#input[@]} -eq 1 ]] && say_verse $input && exit 0;
+
+while [[ $input -ge 0 ]]; do
+    say_verse $input
+    input=$((input-1))
+done
+
+#say_verse $input
 
 exit 0
--- beer-song/beer_song_test.sh
+++ beer-song/beer_song_test.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-set -e
 
 @test 'first_generic_verse' {
     #[[ $BATS_RUN_SKIPPED == true  ]] || skip
-    expected="99 bottles of beer on the wall, 99 bottles of beer.
+    expected="99 bottles of beer on the wall, 99 bottles of beer. 
 Take one down and pass it around, 98 bottles of beer on the wall."
     run bash beer_song.sh 99
     [[ $status -eq 0 ]]
@@ -12,7 +11,7 @@ Take one down and pass it around, 98 bottles of beer on the wall."
 
 @test '3rd_last_generic_verse' {
     [[ $BATS_RUN_SKIPPED == true  ]] || skip
-    expected="3 bottles of beer on the wall, 3 bottles of beer.
+    expected="3 bottles of beer on the wall, 3 bottles of beer. 
 Take one down and pass it around, 2 bottles of beer on the wall."
     run bash beer_song.sh 3
     [[ $status -eq 0 ]]
