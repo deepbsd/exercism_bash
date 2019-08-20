d2832206798d93b291c0272c53640ca85fef35cc
David S. Jackson <deepbsd@yahoo.com>
2019-08-20 14:39:54 -0400

    updated beer song and added darts.  almost done with darts

--- beer-song/beer_song.sh
+++ beer-song/beer_song.sh
@@ -2,7 +2,6 @@
 
 input=("$@")    # snarf the arguments
 
-
 error(){
     echo "1 or 2 arguments expected" && exit 1
 }
@@ -11,57 +10,55 @@ error1(){
     echo "Start must be greater than End" && exit 1
 }
 
-# throw an error if we don't get no arguments
+# throw an error if get too few or too many arguments
 [[ ${#input[@]} -gt 0 && ${#input[@]} -lt 3 ]] || error
 # check that verses are in right order
-if [[ ${#input[@]} -gt 1 && $1 -lt $2 ]]; then
-	error1
-fi
+[[ ${#input[@]} -gt 1 && $1 -lt $2 ]] && error1
 
 say_verse(){
 
-    bottle_num=$1
-    bottle_num1=$((bottle_num-1))
-    bottle_sing="bottle"
-    bottle_plural="bottles"
+    count1=$1
+    count2=$((count1-1))
+    singular="bottle"
+    plural="bottles"
     verse2="Take one down and pass it around,"
     verse3="Go to the store and buy some more,"
 	verse4="Take it down and pass it around,"
  
-   if [[ $bottle_num -gt 2 ]]; then
-        bottles1=${bottle_plural}
-        bottles2=${bottle_plural}
+   if [[ $count1 -gt 2 ]]; then
+        bottles1=${plural}
+        bottles2=${plural}
     fi
 
-    if [[ $bottle_num -eq 2 ]]; then
-        bottles1=${bottle_plural}
-        bottles2=${bottle_sing}
+    if [[ $count1 -eq 2 ]]; then
+        bottles1=${plural}
+        bottles2=${singular}
     fi
  
-    if [[ $bottle_num -eq 1 ]]; then
-        bottle_num1="no more"
+    if [[ $count1 -eq 1 ]]; then
+        count2="no more"
 		verse2=${verse4}
-        bottles1=${bottle_sing}
-        bottles2=${bottle_plural}
+        bottles1=${singular}
+        bottles2=${plural}
     fi
 
-    if [[ $bottle_num -eq 0 ]]; then
-        bottle_num="no more"
-        bottle_num1=99
-        bottles1=${bottle_plural}
-        bottles2=${bottle_plural}
+    if [[ $count1 -eq 0 ]]; then
+        count1="no more"
+        count2=99
+        bottles1=${plural}
+        bottles2=${plural}
         verse2=${verse3}
     fi
 
-
-    verse="${bottle_num} ${bottles1} of beer on the wall, ${bottle_num} ${bottles1} of beer. \n${verse2} ${bottle_num1} ${bottles2} of beer on the wall.\n"
-
+    verse="${count1} ${bottles1} of beer on the wall, ${count1} ${bottles1} of beer. \n${verse2} ${count2} ${bottles2} of beer on the wall.\n"
 
     printf "${verse^}"
 }
 
+# If the range is a single verse
 [[ ${#input[@]} -eq 1 ]] && say_verse $input && exit 0;
 
+# If the range is more than a single verse
 while [[ $input -ge $2 ]]; do
     say_verse $input
     echo
---
+++ darts/darts.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+error(){
+    echo "Must have two numeric arguments" && exit 1
+}
+
+# sanitize input
+[ $# -lt 2 ] && error
+[ "$1" -eq "$1" ] && [ "$2" -eq "$2" ] || error
+
+
+distance=$(echo "sqrt($1^2"+"$2^2)" | bc -l)
+
+if [ $(echo "$distance <= 1.0" | bc -l) -eq 1 ]; then echo 10 && exit 0; fi
+if [ $(echo "$distance <= 5.0" | bc -l) -eq 1 ]; then echo 5 && exit 0; fi
+if [ $(echo "$distance <= 10.0" | bc -l) -eq 1 ]; then echo 1 && exit 0; fi
+if [ $(echo "$distance > 10" | bc -l) -eq 1 ]; then echo 0 && exit 0; fi
+
+
+
+
+
+
