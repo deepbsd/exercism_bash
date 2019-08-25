c6e7e1e6cf562cb7c96bd52dd5ab28ce5835e666
David S. Jackson <deepbsd@yahoo.com>
2019-08-25 14:25:07 -0400

    still dorking with allergies.sh

--- allergies/allergies.sh
+++ allergies/allergies.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
 
+score=$1
+method=$2
+allergies=$3
 
-allergens=('eggs''peanuts' 'shellfish' 'strawberries' 'tomatoes' 'chocolate' 'pollen' 'cats');
+allergens=('eggs' 'peanuts' 'shellfish' 'strawberries' 'tomatoes' 'chocolate' 'pollen' 'cats')
+
+length=${#allergens[@]}
+
+function genlist(){
+    for (( i=0; i<$length; i++))
+    do
+
+        #echo allergens[$((echo ${score}<<$i))]
+
+        echo ${allergens[$i]}
+    done
+
+}
+  
+
+
+list=$(genlist $score)
+
+genlist
+
+echo $list
+
