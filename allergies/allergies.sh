f7b8df1bf54f6a52f4565a77b50968bdac4b0b36
David S. Jackson <deepbsd@yahoo.com>
2019-08-27 11:36:49 -0400

    finished allergies, passing tests now

--- allergies/allergies.sh
+++ allergies/allergies.sh
@@ -6,24 +6,26 @@ allergies=$3
 
 allergens=('eggs' 'peanuts' 'shellfish' 'strawberries' 'tomatoes' 'chocolate' 'pollen' 'cats')
 
-length=${#allergens[@]}
+list=''
 
 function genlist(){
-    for (( i=0; i<$length; i++))
-    do
-
-        #echo allergens[$((echo ${score}<<$i))]
-
-        echo ${allergens[$i]}
+    # get a list of index numbers for allergens
+    for i in "${!allergens[@]}"; do
+        # if the bit for that position is not zero add the allergen
+        num=$(( $score & $(( 1 << $i )) ));
+        if [[ $num -gt 0 ]]; then list+=" ${allergens[$i]}"; fi
     done
-
 }
-  
 
+function allergic_to(){
+    if [[ "$list" =~ "$allergies" ]]; then echo "true"
+    else echo "false"
+    fi
+}
 
-list=$(genlist $score)
+genlist  # do this regardless...
 
-genlist
+if [[ $method == 'list' ]]; then echo $list; fi
+if [[ $method == 'allergic_to' ]]; then allergic_to; fi
 
-echo $list
 
