e966db612c5a7556521683977ee0c7f008effd4e
David S. Jackson <deepbsd@yahoo.com>
2019-09-07 23:52:20 -0400

    almost working...

--- prime-factors/prime_factors.sh
+++ prime-factors/prime_factors.sh
@@ -4,7 +4,7 @@ num=$1
 factor=2
 result=()
 while [ $num -gt 1 ]; do
-    echo "Num: ${num} Factor: ${factor}"
+    echo "Num: ${num} Factor: ${factor}"  # remove later
     if [  $((num % factor)) -eq 0 ]; then
         result+=$(factor)
         (( num /= factor ))
