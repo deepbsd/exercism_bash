e9dd64db5efe3667858d9e37afb4845d48f3d5e7
David S. Jackson <deepbsd@yahoo.com>
2019-09-13 17:19:57 -0400

    tried a different solution. no big difference except for legibility

--- proverb/proverb1.sh
+++ proverb/proverb1.sh
@@ -3,11 +3,13 @@
 main(){
     local items=("$@")
 
-    for ((i=0; i < $# - 1; i++)); do
+    for ((i=0; i < $# - 1; i++))
+    do
         echo "For want of a ${items[i]} the ${items[i+1]} was lost."
     done
 
-    if [ $# -gt 0 ]; then
+    if [ $# -gt 0 ]
+    then
         echo "And all for the want of a ${items[0]}."
     fi
 }
