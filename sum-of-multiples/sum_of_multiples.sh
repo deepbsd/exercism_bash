1798f9fa57d746b83809c1c26252c8a6a8a2b2b8
David S. Jackson <deepbsd@yahoo.com>
2020-03-25 00:59:45 -0400

    solved problem, on to next test

--- sum-of-multiples/sum_of_multiples.sh
+++ sum-of-multiples/sum_of_multiples.sh
@@ -7,16 +7,13 @@ main(){
     factors_arr=()
     for number in "$@" ; do
         for (( i=1; i<$limit; i++ )) ; do
-            echo "pass for  $number"
             [[ $(( $i % $number )) == 0 ]] && \
-                # Problem! False positive for '5' and '25' or '25' in '125'
-                # Might have to loop over each element in ${factors_arr}
-                [[ ! ${factors_arr[@]} =~ "$i" ]] && \
+                [[ ! ${factors_arr[@]} =~ " $i " ]] && \
                 factors_arr+=( "$i" )
         done
     done
 
-    echo ${factors_arr[@]}
+    #echo ${factors_arr[@]}
 
     for factor in "${factors_arr[@]}" ; do
         let sum=sum+factor
@@ -29,6 +26,6 @@ main(){
 
 }
 
-main 100 3 5
-#main "$@"
+#main 100 3 5
+main "$@"
 
