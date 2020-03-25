494371f0f2c96cfe2dbcca4bb8ee367611abca2b
David S. Jackson <deepbsd@yahoo.com>
2020-03-25 11:02:03 -0400

    all tests passing with good time complexity now; removed 'includes' function and simply removed dupes from array

--- sum-of-multiples/sum_of_multiples.sh
+++ sum-of-multiples/sum_of_multiples.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-contains_element(){
-    local el match=$1
-    shift
-    for el; do [[ "$el" == "$match" ]] && return 0; done
-    return 1
-}
-
 main(){
     sum=0
     limit=$1
@@ -15,15 +8,13 @@ main(){
     for number in "$@" ; do
         [[ $number -eq 0 ]] && continue
         for (( i=1; i<$limit; i++ )) ; do
-            [[ $(( $i % $number )) == 0 ]] && \
-                #[[ ! ${factors_arr[@]} =~ " $i " ]] && \
-                ! contains_element $i "${factors_arr[@]}" && \
-                factors_arr+=( "$i" )
+            [[ $(( $i % $number )) == 0 ]] && factors_arr+=( "$i" )
         done
     done
-
-    #echo ${factors_arr[@]}
-
+    # remove dupes
+    factors_arr=($(for element in "${factors_arr[@]}"; \
+    do echo "$element";done|sort|uniq|xargs))
+    # get the sum
     for factor in "${factors_arr[@]}" ; do
         let sum=sum+factor
     done
@@ -31,6 +22,5 @@ main(){
     echo $sum
 }
 
-#main 4 3 0
 main "$@"
 
