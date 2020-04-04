b64d519a6dbe96965a47d3aad6dde8960f0b89d4
David S. Jackson <deepbsd@yahoo.com>
2020-04-04 12:29:48 -0400

    seems to be passing all tests

--- run-length-encoding/run_length_encoding.sh
+++ run-length-encoding/run_length_encoding.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 
-
 decode(){
-    echo "decoding"
+    input=$1; output=""; count=0; chr=""
+    while [[ -n $input ]]; do
+        [[ $input =~ ([0-9]*)([^0-9]) ]] && \
+            count=${BASH_REMATCH[1]} && \
+            if [[ -z $count ]]; then count=1; fi &&\
+            chr=${BASH_REMATCH[2]} && \
+            output+="$(repeat_let "$count" "$chr")" && \
+            length=${#BASH_REMATCH[0]} && \
+            input=${input:length}
+    done
+    echo "$output"
 }
 
 count_let(){
     count=$1; chr=$2
-    ((count == 1)) && count=""
+    [[ "$count" -eq 1 ]] && count=""
     echo "$count$chr"
+}
+
+repeat_let(){
+    count=$1; chr=$2; output=""
+    for ((c=1; c<=$count; c++)); do 
+        result+="$chr"
+    done
+    echo "$result"
 }
 
 encode(){
@@ -31,13 +48,9 @@ encode(){
 }
 
 main(){
-    method=$1
-    input=$2
+    method=$1; input=$2
     [[ "$method" == 'encode' ]] && encode "$input"
     [[ "$method" == 'decode' ]] && decode "$input"
 }
 
 main "$@"
-#main 'AAABBBBCCCCC'
-#main ''
-#main 'XYZ'
