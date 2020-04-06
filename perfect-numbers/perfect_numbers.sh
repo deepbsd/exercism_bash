dcd36c56a10574fb6a5ea73dd9b3da29092487e0
David S. Jackson <deepbsd@yahoo.com>
2020-04-06 13:49:02 -0400

    changed solution to final solution, down to ~ 3.5min of execution time. All tests pass

--- perfect-numbers/perfect_numbers.sh
+++ perfect-numbers/perfect_numbers.sh
@@ -1,41 +1,26 @@
 #!/usr/bin/env bash
 
-declare -a factors
-
 error(){
-echo "Classification is only possible for natural numbers."
-exit 1
+    echo "Classification is only possible for natural numbers."
+    exit 1
 }
 
-get_factors(){
-    local number=$1; 
-    for ((n=1; n<=$number; n++)); do
-        [[ "$n" -eq "$number" ]] && continue
-        [[ $((number%n)) -eq 0 ]] && factors+=("$n") 
+sum_factors(){
+    local sum=0
+    for ((n=1; n*2<=$1; n++)); do
+        (( $1 % n == 0 && (sum+=n) )) 
     done
-}
-
-get_sum(){
-    sum=0
-    for num in "${factors[@]}"; do let sum+="$num"; done
     echo "$sum"
-}
-
-is_prime(){
-    check=$(((($1**2)+17)%12))
-    [[ $check -eq 6 ]] && return 0
-    return 1
 }
 
 main(){
     input_num=$1
-    [[ $input_num -le 0 ]]  && error && exit 1
-    [[ $(is_prime "$input_num") ]] && echo "deficient" && exit 0
-    get_factors "$input_num"
-    sum=$(get_sum "$input_num")
-    [[ "$sum" -eq "$input_num" ]] && echo "perfect" && exit 0
-    [[ "$sum" -lt "$input_num" ]] && echo "deficient" && exit 0
-    echo "abundant" && exit 0
+    [[ $input_num -le 0 ]] && error 
+    sum=$(sum_factors "$input_num")
+    [[ "$sum" -eq "$input_num" ]] && echo "perfect"
+    [[ "$sum" -lt "$input_num" ]] && echo "deficient"
+    [[ "$sum" -gt "$input_num" ]] && echo "abundant" 
+    exit 0
 }
 
 main "$@"
