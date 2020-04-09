13e883e6c2c3fc72fbc7bff3d76b75e9e32de4a6
David S. Jackson <deepbsd@yahoo.com>
2020-04-09 15:44:42 -0400

    all is working except for reversing array at 10000

--- secret-handshake/secret_handshake.sh
+++ secret-handshake/secret_handshake.sh
@@ -3,30 +3,18 @@
 actions=( 'wink' 'double blink' 'close your eyes' 'jump' )
 reverse_actions=( 'jump' 'close your eyes' 'double blink' 'wink' )
 
+
 main(){
-    result=""
-    count=0
-    num=$1
-    while [[ $(( num >> 1 )) -gt 0 ]]; do
+    result=()
+    count=0; num=$1; bits=''
+    while [[  $num -ne 0 ]]; do
+        (( $num & 1 )) && bits="1$bits" || bits="0$bits"
+        [[ "${bits:0:1}" == "1" ]] && result+=("${actions[$count]}") 
+        #echo "count: $count  num: $num  bits: $bits result: $result"
         num=$(( num >> 1 )) && ((count++))
-        result="${actions[$count]}"
     done
-    if [[ $(( num >> 1 )) -eq 0 ]]; then result="${actions[$count]}"; fi
-    echo "$result"
+    [[ "${#result}" -gt 1 ]] && printf ",%s" "${result[@]}" | \
+        sed 's/^,//g' || echo "${result[@]}"
 }
 
-#old_main(){
-#    result=()
-#    binary=$(bc <<< "obase=2;$1")    
-#    length=${#binary}
-#    #echo "binary: $binary"
-#    reversebin=$( echo "$binary" | rev )
-#    echo "reverse: $reversebin"
-#    for ((x=0; x<${#reversebin}; x++)); do
-#        echo "action: ${actions[$x]} x: $x"
-#        [[ "${reversebin[$x]}" -eq 1 ]] && result+=( "${actions[$x]}" )
-#    done
-#
-#    echo "${result[@]}"
-#}
 main "$@"
