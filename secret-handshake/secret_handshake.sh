2399c3f517e4d03e5360e769361cb82bd4f8bf28
David S. Jackson <deepbsd@yahoo.com>
2020-04-08 20:38:54 -0400

    first couple tests passing

--- secret-handshake/secret_handshake.sh
+++ secret-handshake/secret_handshake.sh
@@ -3,19 +3,30 @@
 actions=( 'wink' 'double blink' 'close your eyes' 'jump' )
 reverse_actions=( 'jump' 'close your eyes' 'double blink' 'wink' )
 
-
 main(){
-    result=()
-    binary=$(bc <<< "obase=2;$1")    
-    length=${#binary}
-    #echo "binary: $binary"
-    reversebin=$( echo "$binary" | rev )
-    echo "reverse: $reversebin"
-    for ((x=0; x<${#reversebin}; x++)); do
-        echo "action: ${actions[$x]} x: $x"
-        [[ "${reversebin[$x]}" -eq 1 ]] && result+=( "${actions[$x]}" )
+    result=""
+    count=0
+    num=$1
+    while [[ $(( num >> 1 )) -gt 0 ]]; do
+        num=$(( num >> 1 )) && ((count++))
+        result="${actions[$count]}"
     done
-
-    echo "${result[@]}"
+    if [[ $(( num >> 1 )) -eq 0 ]]; then result="${actions[$count]}"; fi
+    echo "$result"
 }
+
+#old_main(){
+#    result=()
+#    binary=$(bc <<< "obase=2;$1")    
+#    length=${#binary}
+#    #echo "binary: $binary"
+#    reversebin=$( echo "$binary" | rev )
+#    echo "reverse: $reversebin"
+#    for ((x=0; x<${#reversebin}; x++)); do
+#        echo "action: ${actions[$x]} x: $x"
+#        [[ "${reversebin[$x]}" -eq 1 ]] && result+=( "${actions[$x]}" )
+#    done
+#
+#    echo "${result[@]}"
+#}
 main "$@"
