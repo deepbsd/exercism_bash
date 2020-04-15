48d851458d16e1745ddd78ac9fde9bef9e701b66
David S. Jackson <deepbsd@yahoo.com>
2020-04-15 12:55:55 -0400

    just starting to work with slices...

--- sublist/sublist.sh
+++ sublist/sublist.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 main(){
+    first=$(echo $1 | tr -d "[\[\],]")
+    second=$(echo $2 | tr -d "[\[\],]")
+    [[ -z $first  || -z $second ]] && echo 'hey' && exit 0
 
+    [[ "${first[@]:0:${#first}-1}" == "${second[@]:0:${#second}-1}" ]] && echo 'hey'
+    
 }
 main "$@"
