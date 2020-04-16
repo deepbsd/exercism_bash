507337380b478903658594493ecc584a363214b6
David S. Jackson <deepbsd@yahoo.com>
2020-04-16 14:24:11 -0400

    still just getting started

--- diamond/diamond.sh
+++ diamond/diamond.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
+
 main(){
     letter=$1
     let_arr=()
     output=()
-    for letr in [a..${letter}]; do
-        let_arr+=( "$letr" )
+    for letter in $(IFS=' '; eval echo {A..$letter}); do
+        let_arr+=( "$letter" )
     done
     
-    single=true; space=""; line=""
+    single=true; local space=""; local line=""
     for ((i=0; i<${#let_arr}; i++)); do
         [[ "$single" ]] && line+="${let_arr[$i]}" && output+=( "$line" )
         if [[ ! "$single" ]]; then 
             space=" "; single=false
             line+="${let_arr[$i]}${space}${let_arr[$i]}"
             output+=( "$line" )
+            echo "$output"
         fi
     done
 
     for (( i="${#let_arr}-1"; i>=0; i-- )); do
-       echo          
+       line="${space}${let_arr[$i]}${space}"
+       echo "$output"
     done
 
 
