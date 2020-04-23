d4d82e9d95068b7ba56744880a83661fbd9a136c
Erik Dubois <erik.dubois@gmail.com>
2020-04-23 12:37:24 -0400

    just starting matching brackets, trying out some ideas

--- matching-brackets/matching_brackets.sh
+++ matching-brackets/matching_brackets.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
+declare -A pairs=( ['{']='}' ['[']=']' ['(']=')' )
+declare -a openers=( '{' '[' '(' )
+declare -a closers=( '}' ']' ')' )
+declare -a stack=()
+
+say_true(){ echo true && exit 0; }
+say_false(){ echo false && exit 0; }
+error(){ echo 'invalid input' && exit 1; }
+
+index_of(){
+    for (( i=0; i<${#openers[@]}; i++ )){
+        [[ $1 == ${openers[$i]} ]] && echo $i
+    }
+}
+
 main(){
-    echo
+    input=$1
+    for ((c=0; c<${#input[@]}; c++)){
+        [[ [\[\]\(\){}] =~ $c ]] && stack+=( $c )
+    }
+    echo "${stack[@]}" 
+
 }
 main "$@"
