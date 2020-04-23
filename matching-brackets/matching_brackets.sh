11ce6c2ad0ea0e6e990f62cede2b82d4a029d967
David S. Jackson <deepbsd@yahoo.com>
2020-04-23 18:35:12 -0400

    playing with checking characters stripped from stack

--- matching-brackets/matching_brackets.sh
+++ matching-brackets/matching_brackets.sh
@@ -20,7 +20,14 @@ main(){
     for ((c=0; c<${#input}; c++)){
         [[ [\(\)[]{}] =~ "${input:$c:1}" ]] && stack+=( "${input:$c:1}" )
     }
-    echo "${stack[@]}" 
+
+    for c in $( echo ${stack[@]} | rev ) ; do
+        if [[ ${closers[@]} =~ "$c" ]]; then
+            if [[ ${closers[$(index_of $c)]} -eq ${pairs[${openers[$(index_of $c)]}]} ]]; then
+                unset 'stack[-1]' && unset 'stack[-1]'
+            fi
+        fi
+    done
 
 }
 main "$@"
