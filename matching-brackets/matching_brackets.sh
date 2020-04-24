680f7c61d393a4fd289001cecd3e3ffe395466a7
David S. Jackson <deepbsd@yahoo.com>
2020-04-24 16:14:41 -0400

    not quite working yet...

--- matching-brackets/matching_brackets.sh
+++ matching-brackets/matching_brackets.sh
@@ -19,16 +19,21 @@ main(){
     for ((c=0; c<${#input}; c++)){
         [[ [\(\)[]{}] =~ "${input:$c:1}" ]] && stack+=( "${input:$c:1}" )
     }
-
-    for ((i=${#stack}-1; i>=0; i--)); do
-        c=${stack:$i:1}
-        if [[ ${closers[@]} =~ "$c" ]]; then
+    echo "stack: ${stack[@]} length: ${#stack[@]}"
+    for (( i=0; i<${#stack[@]}-1; i++ )); do
+        c=${stack[$i]}
+        echo "c: $c  closers: ${closers[@]}"
+        echo "closer: ${closers[$(index_of $c)]}   pairs[closer]: ${pairs[${openers[$(index_of $c)]}]}"
+        if [[ "${closers[@]}" =~ "$c" ]]; then
+            echo 'closer'
             if [[ ${closers[$(index_of $c)]} == ${pairs[${openers[$(index_of $c)]}]} && 
-                ${stack:$i-1:1} == ${openers:$(index_of $c)} ]]; then
+                ${stack[$i-1]} == ${openers[$(index_of $c)]} ]]; then
+                echo 'unsetting...'
                 unset 'stack[-1]' && unset 'stack[-1]'
             fi
         fi
     done
+    echo "stack length: ${#stack[@]}"
     [[ ${#stack[@]} -eq 0 ]] && say_true || say_false
 }
 main "$@"
