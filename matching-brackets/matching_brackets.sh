76b121eb6961a5bb3fe6396693c0b47df2a8f7b5
David S. Jackson <deepbsd@yahoo.com>
2020-04-25 18:18:58 -0400

    tried some more tests

--- matching-brackets/matching_brackets.sh
+++ matching-brackets/matching_brackets.sh
@@ -22,17 +22,17 @@ main(){
     #stack_dup+=( "${stack[@]}" )
     #echo "stack_dup: ${stack_dup[@]} length: ${#stack_dup[@]}"
     [[ ${stack:0:1} != "" && ${closers[@]} =~ ${stack:0:1} ]] && say_false
-    #echo "stack: ${stack[@]} length: ${#stack[@]}"
+    echo "stack: ${stack[@]} length: ${#stack[@]}"
     for (( i=0; i<${#stack[@]}; i++ )); do
         c=${stack[$i]}
-        #echo "index: $i  c: $c  closers: ${closers[@]} closers_c_idx: ${closers[$(index_of $c)]}"
-        #echo "closer: ${closers[$(index_of $c)]}   pairs[closer]: ${pairs[${openers[$(index_of $c)]}]}"
-        #echo "previous in stack: ${stack[$i-1]}"
+        echo "index: $i  c: $c  closers: ${closers[@]} closers_c_idx: ${closers[$(index_of $c)]}"
+        echo "closer: ${closers[$(index_of $c)]}   pairs[closer]: ${pairs[${openers[$(index_of $c)]}]}"
+        echo "previous in stack: ${stack[$i-1]}"
         if [[ "${closers[@]}" =~ "$c" ]]; then
-            #echo 'closer'
+            echo 'closer'
             if [[ ${closers[$(index_of $c)]} == ${pairs[${openers[$(index_of $c)]}]} && 
                 ${stack[$i-1]} == ${openers[$(index_of $c)]} ]]; then
-                #echo "unsetting index: $i and $((i-1))"
+                echo "unsetting index: $i and $((i-1))"
                 unset "stack[$i]" && unset "stack[$((i-1))]"
             fi
         fi
