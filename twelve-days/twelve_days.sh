121b4d762ae6b1e2ab0d6e7294801bfa7c8207a4
David S. Jackson <deepbsd@yahoo.com>
2020-04-30 09:57:42 -0400

    all tests passing for twelve-days

--- twelve-days/twelve_days.sh
+++ twelve-days/twelve_days.sh
@@ -4,25 +4,25 @@ days=( null 'first' 'second' 'third' 'fourth' 'fifth' 'sixth'
     'seventh' 'eighth' 'ninth' 'tenth' 'eleventh' 'twelfth' )
 
 gifts=( null 'and a Partridge in a Pear Tree' 'two Turtle Doves' 
-    'three French Hens' 'four Calling Birds' 'five Golden Rings'
-    'six Geese-A-Laying' 'seven Swans-A-Swimming' 'eight Maids-A-Milking'
-    'nine Ladies Dancing' 'ten Lords-A-Leaping' 'eleven Pipers Piping'
+    'three French Hens' 'four Calling Birds' 'five Gold Rings'
+    'six Geese-a-Laying' 'seven Swans-a-Swimming' 'eight Maids-a-Milking'
+    'nine Ladies Dancing' 'ten Lords-a-Leaping' 'eleven Pipers Piping'
 'twelve Drummers Drumming' )
 
 function say_prefix(){
     day_num=$1
-    echo "On the ${days[day_num]} day of Christmas my true love gave to me: "
-    say_gifts "$day_num"
+    [[ $day_num -eq 1 ]] && \
+        echo "On the ${days[$day_num]} day of Christmas my true love gave to me: $(say_gifts $day_num)" | sed 's/and a Partridge/a Partridge/g'\
+    || echo "On the ${days[$day_num]} day of Christmas my true love gave to me: $(say_gifts $day_num)"
 }
 
 function say_gifts(){
     verse_num=$1; verse=''
     while [[ $verse_num -ge 1 ]]; do
-        [[ $verse_num -eq 1 ]] && verse+="and ${gifts[$verse_num]}" || verse+="${gifts[$verse_num]}"
-        echo "$verse."
+        [[ $verse_num -eq 1 ]] && verse+="${gifts[$verse_num]}" || verse+="${gifts[$verse_num]}, "
         ((verse_num--))
     done
-
+    echo "$verse."
 }
 
 main(){
