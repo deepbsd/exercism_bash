df9013713fb05f27b4b1c8ea0dce43eb00c24b29
David S. Jackson <deepbsd@yahoo.com>
2020-04-30 09:21:49 -0400

    just getting started on Twelve Days

--- twelve-days/twelve_days.sh
+++ twelve-days/twelve_days.sh
@@ -12,16 +12,24 @@ gifts=( null 'and a Partridge in a Pear Tree' 'two Turtle Doves'
 function say_prefix(){
     day_num=$1
     echo "On the ${days[day_num]} day of Christmas my true love gave to me: "
+    say_gifts "$day_num"
 }
 
 function say_gifts(){
-    echo
+    verse_num=$1; verse=''
+    while [[ $verse_num -ge 1 ]]; do
+        [[ $verse_num -eq 1 ]] && verse+="and ${gifts[$verse_num]}" || verse+="${gifts[$verse_num]}"
+        echo "$verse."
+        ((verse_num--))
+    done
 
 }
 
 main(){
     start=$1; end=$2
-    say_prefix $start
+    for ((v=$start; v<=$end; v++)){
+        say_prefix $v
+    }
 
 }
 main "$@"
