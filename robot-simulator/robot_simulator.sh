868782b7d2ed551710c4f24e71c57b0c63d7e07e
David S. Jackson <deepbsd@yahoo.com>
2020-05-03 15:54:13 -0400

    nitpicking on robot-simulator

--- robot-simulator/robot_simulator.sh
+++ robot-simulator/robot_simulator.sh
@@ -2,14 +2,13 @@
 
 declare -a directions=( 'north' 'east' 'south' 'west' )
 declare -A bearings=( [north]=0 [east]=1 [south]=2 [west]=3 )
-function direction_err(){ echo "invalid direction"; exit 1; }
 
 main(){
     [[ -z $1 ]] && EW=0 || EW=$1; 
     [[ -z $2 ]] && NS=0 || NS=$2; 
     [[ -z $3 ]] && bearing='north' || bearing=$3
     [[ -z $4 ]] && args='' || args=$4
-    [[ "${!bearings[@]}" =~ $bearing ]] || direction_err
+    [[ "${!bearings[@]}" =~ $bearing ]] || { echo "invalid direction"; exit 1; }
     [[ -n $4 ]] && \
         for ((c=0; c<${#args}; c++)); do
         case ${args:$c:1} in
