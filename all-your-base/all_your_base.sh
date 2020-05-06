12f2b1bff39694a1cfb11cf24d2a906f533e47aa
David S. Jackson <deepbsd@yahoo.com>
2020-05-05 22:02:54 -0400

    still working on it

--- all-your-base/all_your_base.sh
+++ all-your-base/all_your_base.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-main(){
-    ibase=$1; obase=$3; number=$( echo "$2" | sed 's/ //g' ); total=0
-    length=$((${#number}))
-    #echo "number: $number length: $length"
-    for ((i=0; i<${#number}; i++)){
-        let total+="${number:$i:1} * $(($obase**(($length-1))))"
+toBase10(){
+    number=$1; total=0; outBase=10; inBase=$2
+    length=$((${#number[@]}))
+    
+    for i in "${number[@]}"; {
+        total+="$(( $i*($inBase**$length) ))"
         ((length--))
-        #echo "total: $total i: $i length: $length ibase: $ibase obase: $obase"
     }
     echo "$total"
+}
+
+fromBase10(){
+    number=$1; result=0; outbase=$2
+    result+=( $(( $number % $outbase )) )
+    
+    fromBase10 $number/$obase $outbase
+}
+
+main(){
+    inBase=$1; outBase=$3; number=$2
+
+    inBase10=$(toBase10 "$number" "$inbase")
+    targetBase=()
+    echo $(toBase10 "$targetBase" "$inBase10")
 }
 main "$@"
