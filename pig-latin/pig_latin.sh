d477eb8e511b854582c70c3bf4296a5ebcee9551
David S. Jackson <deepbsd@yahoo.com>
2020-05-09 14:44:40 -0400

    alltests passing on pig-latin except last two tests

--- pig-latin/pig_latin.sh
+++ pig-latin/pig_latin.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+translate(){
+    word=$1
+    [[ ${word:0:1} =~ [aeiou] ]] && echo "${word}ay" && return
+    [[ ${word:0:3} =~ (squ|thr|sch) ]] && echo "${word:3}${word:0:3}ay" && return
+    [[ ${word:0:2} =~ (qu|rh|ch|sh|sk|th) ]] && echo "${word:2}${word:0:2}ay" && return
+    [[ ${word:0:2} =~ (yt|xr) ]] && echo "${word}ay" && return
+    echo "${word:1}${word:0:1}ay" && return
+}
+
 main(){
-    echo
+    [[ "$#" -gt 1 ]] && for ((w=0; w<$#; w++)); do translate $w; done && exit 0
+    translate $1
 }
 main "$@"
