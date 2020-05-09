9bdb7a7c93acacacb914cdb826385acefa630c40
David S. Jackson <deepbsd@yahoo.com>
2020-05-09 15:00:04 -0400

    working on last two tests

--- pig-latin/pig_latin.sh
+++ pig-latin/pig_latin.sh
@@ -2,15 +2,16 @@
 
 translate(){
     word=$1
-    [[ ${word:0:1} =~ [aeiou] ]] && echo "${word}ay" && return
-    [[ ${word:0:3} =~ (squ|thr|sch) ]] && echo "${word:3}${word:0:3}ay" && return
-    [[ ${word:0:2} =~ (qu|rh|ch|sh|sk|th) ]] && echo "${word:2}${word:0:2}ay" && return
-    [[ ${word:0:2} =~ (yt|xr) ]] && echo "${word}ay" && return
-    echo "${word:1}${word:0:1}ay" && return
+    [[ ${word:0:1} =~ [aeiou] ]] && echo -n "${word}ay" && exit
+    [[ ${word:0:3} =~ (squ|thr|sch) ]] && echo -n "${word:3}${word:0:3}ay" && exit
+    [[ ${word:0:2} =~ (qu|rh|ch|sh|sk|th) ]] && echo -n "${word:2}${word:0:2}ay" && exit
+    [[ ${word:0:2} =~ (yt|xr) ]] && echo -n "${word}ay" && return
+    echo -n "${word:1}${word:0:1}ay" && return
 }
 
 main(){
-    [[ "$#" -gt 1 ]] && for ((w=0; w<$#; w++)); do translate $w; done && exit 0
+    #echo "how many: $#"
+    [[ "$#" -gt 1 ]] && for w in "$@"; do translate "$w"; done #&& exit 0
     translate $1
 }
 main "$@"
