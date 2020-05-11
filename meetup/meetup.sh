1f862b8c9fbf5ab55f2805366023ce4a6c3d8cc7
David S. Jackson <deepbsd@yahoo.com>
2020-05-11 15:16:53 -0400

    just starting an idea for meetup

--- meetup/meetup.sh
+++ meetup/meetup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+declare -A weekdays=( [Sunday]=0  [Monday]=1 [Tuesday]=2 [Wednesday]=3 [Thursday]=4 [Friday]=5 [Saturday]=6 )
+
 main(){
-    echo
+    yr=$1; month=$2; which=$3; day=$4
 }
 main "$@"
