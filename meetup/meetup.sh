a186358318ab3e73a5ca16fc8926e1e7aa47ab65
Erik Dubois <erik.dubois@gmail.com>
2020-05-13 23:56:28 -0400

    trying a different idea from js

--- meetup/meetup.sh
+++ meetup/meetup.sh
@@ -2,7 +2,9 @@
 
 declare -A weekdays=( [Sunday]=0  [Monday]=1 [Tuesday]=2 [Wednesday]=3 [Thursday]=4 [Friday]=5 [Saturday]=6 )
 
-declare -A ranges=( [1st]=arrayOfDates[0] [teenth]=arrayOfDates.find( date => date > 12 ) [2nd]=arrayOfDates[1]  [3rd]=arrayOfDates[2] [4th]=arrayOfDates[3] [5th]=arrayOfDates[4] [last]=arrayOfDates[arrayOfDates.length-1] )
+declare -A ranges=( [1st]=arrayOfDates[0] [teenth]=$(arrayOfDates.find( date => date > 12)) 
+    [2nd]=arrayOfDates[1]  [3rd]=arrayOfDates[2] [4th]=arrayOfDates[3] [5th]=arrayOfDates[4] 
+    [last]=arrayOfDates[arrayOfDates.length-1] )
 
 
 
