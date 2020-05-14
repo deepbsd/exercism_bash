7af83d104db5996d11f6a90df09501eb8d21a7f2
Erik Dubois <erik.dubois@gmail.com>
2020-05-13 23:54:05 -0400

    did the last commit take?

--- meetup/meetup.sh
+++ meetup/meetup.sh
@@ -4,6 +4,8 @@ declare -A weekdays=( [Sunday]=0  [Monday]=1 [Tuesday]=2 [Wednesday]=3 [Thursday
 
 declare -A ranges=( [1st]=arrayOfDates[0] [teenth]=arrayOfDates.find( date => date > 12 ) [2nd]=arrayOfDates[1]  [3rd]=arrayOfDates[2] [4th]=arrayOfDates[3] [5th]=arrayOfDates[4] [last]=arrayOfDates[arrayOfDates.length-1] )
 
+
+
 main(){
     yr=$1; month=$2; which=$3; day=$4
 }
