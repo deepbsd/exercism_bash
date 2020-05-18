a420904087f11fd402b718de914046d7366f5986
David S. Jackson <deepbsd@yahoo.com>
2020-05-18 01:49:21 -0400

    making progress

--- meetup/meetup.sh
+++ meetup/meetup.sh
@@ -6,26 +6,24 @@ declare -A weekdays=( [sunday]=0  [monday]=1 [tuesday]=2 [wednesday]=3 [thursday
 #    [2nd]=arrayOfDates[1]  [3rd]=arrayOfDates[2] [4th]=arrayOfDates[3] [5th]=arrayOfDates[4] 
 #    [last]=arrayOfDates[arrayOfDates.length-1] )
 
-declare -a instances_in_month=()
+declare -a array_of_dates=()
 
 get_weekday_dates(){
     indate=$(date +%F -d "$1")
     for n in $(seq 1 31); do
-        [[ $(date +%w -d "$indate") == ${weekdays[$2]} ]] && \
-            instances_in_month+=( "$indate" ) && \
-            indate=$(date +%F -d "$indate+$n day")
-        echo "indate: $indate"
+        if [[ $(date +%w -d "$indate") == ${weekdays[$2]} ]] ; then
+            array_of_dates+=( "$indate" ) 
+        fi
+        indate=$(date -d "$indate + 1 day")
     done
-
-    echo "${instances_in_month[@]}"
+    echo "instances:  ${array_of_dates[@]}"
 }
-
 
 main(){
     yr=$1; month=$2; which=$3; weekday=${4,,}
     firstDate_of_month=$(date -d "${yr}-${month}-01")
 
-    get_weekday_dates $firstDate_of_month $weekday
+    get_weekday_dates "$firstDate_of_month" "$weekday"
 
     #first_weekday=${weekdays[$(echo $firstDate_of_month | cut -d' ' -f1)]}
     #echo "first_weekday: ${first_weekday}"
