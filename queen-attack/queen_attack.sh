36e1715ef6989a886fa194d6c7e34aaaf9083322
David S. Jackson <deepbsd@yahoo.com>
2020-05-20 15:34:40 -0400

    just starting on queen-attack

--- queen-attack/queen_attack.sh
+++ queen-attack/queen_attack.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 main(){
-    echo
+    white=$2; black=$4
+    w_row=${white:0:1}; w_col=${white:2:1}
+    b_row=${black:0:1}; b_col=${black:2:1}
+    echo "white: ${white} black: ${black} "
+    echo "w_row: ${w_row} w_col: ${w_col} "
 }
-main "@"
+main "$@"
