3835700947d4825e3755ab7d55a18bdb3d9d0f9c
David S. Jackson <deepbsd@yahoo.com>
2020-05-20 23:52:08 -0400

    finished queen-attack

--- queen-attack/queen_attack.sh
+++ queen-attack/queen_attack.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 
+abs(){ echo $(( $1 < 0 ? -$1 : $1 )); } # get absolute value
+
 main(){
-    white=$2; black=$4
-    w_row=${white:0:1}; w_col=${white:2:1}
-    b_row=${black:0:1}; b_col=${black:2:1}
-    echo "white: ${white} black: ${black} "
-    echo "w_row: ${w_row} w_col: ${w_col} "
+
+    local -i w_row w_col b_row b_col
+
+    while getopts :w:b: opt; do
+        case $opt in
+            w) IFS=, read -r w_row w_col <<< "$OPTARG" ;;
+            b) IFS=, read -r b_row b_col <<< "$OPTARG" ;;
+            *) : ;;  #ignore invalid
+        esac
+    done
+    # Handle errors
+    [[ $w_row -lt 0 || $b_row -lt 0 ]] && echo "row not positive" && exit 1
+    [[ $w_col -lt 0 || $b_col -lt 0 ]] && echo "column not positive" && exit 1
+    [[ $w_row -gt 7 || $b_row -gt 7 ]] && echo "row not on board" && exit 1
+    [[ $w_col -gt 7 || $b_col -gt 7 ]] && echo "column not on board" && exit 1
+    [[ $w_col == $b_col && $w_row == $b_row ]] && echo "cannot occupy same position" && exit 1
+    # diagonals
+    local -i diag1=$( abs $(( w_row - b_row )) )
+    local -i diag2=$( abs $(( w_col - b_col )) )
+    # can attack
+    [[ $w_row == $b_row || $w_col == $b_col ]] && echo true && exit 0
+    [[ $diag1 == 0 || $diag2 == 0 || $diag1 == $diag2 ]] && echo true && exit 0
+
+    echo false
 }
 main "$@"
