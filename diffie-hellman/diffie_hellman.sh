63c62428bd6824b1dc1a9f6721e3dcf279e6c034
David S. Jackson <deepbsd@yahoo.com>
2020-05-23 18:25:25 -0400

    still refining code on diffie, all tests still passing

--- diffie-hellman/diffie_hellman.sh
+++ diffie-hellman/diffie_hellman.sh
@@ -3,11 +3,9 @@
 main(){
     case $1 in
         privateKey ) privateKey $2   ;;
-        publicKey ) pub_sec $2 $3 $4 ;;
-        secret ) pub_sec $2 $3 $4    ;;
+        publicKey|secret ) pub_sec $2 $3 $4 ;;
         * ) echo "invalid input" && exit 1 ;;
-    esac
-    exit 0
+    esac && exit 0
 }
 
 privateKey(){ echo $(( $RANDOM % ($1 - 2) + 2 )); }
