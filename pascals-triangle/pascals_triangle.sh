0e83a90fb6c533c4ca4d04e59a74b1a3ffb4f8f8
David S. Jackson <deepbsd@yahoo.com>
2020-05-26 21:53:13 -0400

    evolving

--- pascals-triangle/pascals_triangle.sh
+++ pascals-triangle/pascals_triangle.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-main(){
+generateRows(){
+    declare -i num=$1 i
+    declare -a rows
+    for ((i=0; i<$num; i++)); do
+        declare -a newRow
+        for ((col=0; col<=$i; col++)); do
+            if ( $col==$i || $col==0 ); then { newRow+=( 1 ); } 
+            else { newRow+=(rows[i-1][col-1]+rows[i-1][col]); } 
+            fi
+        done
+        rows+=(newRow)
+    done
+    echo "${rows[@]}"
+}
 
-    constructor(num){
-        this.rows = this.generateRows(num);
-        this.lastRow = this.rows[num-1];
-    }
-    generateRows(num){
-        let rows = [];
-        for (let i=0; i<num; i++){
-            let newRow = [];
-            for (let col=0; col<=i; col++){
-                if (col===i || col===0) { newRow.push(1); }
-                else { newRow.push(rows[i-1][col-1]+rows[i-1][col]); }
-            }
-            rows.push(newRow);
-        }
-        return rows;
-    }
-    echo
+main(){
+    generateRows "$1"
+    #constructor(num){
+    #    this.rows = this.generateRows(num);
+    #    this.lastRow = this.rows[num-1];
+    #}
 }
 
 main "$@"
