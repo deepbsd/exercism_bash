9f23ecbbe61373b4cc785105c9e1aa6afba3eaa9
David S. Jackson <deepbsd@yahoo.com>
2020-05-30 22:32:52 -0400

    just trying to clean up code on pascals triangle

--- pascals-triangle/pascals_triangle.sh
+++ pascals-triangle/pascals_triangle.sh
@@ -6,17 +6,17 @@ makeSpace(){
 
 main(){
     declare -i num=$1; space=$(makeSpace "$num")
-    declare -A row=( [0,1]=1 )
+    declare -A rows=( [0,1]=1 )
     for ((i=1; i<=$num; i++)); do
-        rows=()
+        cols=()
         for ((j=1; j<=i; j++)); do
             a1=$((i-1)); a2=$((j-1)); a="$a1,$a2"
             b1=$((i-1)); b2=$j; b="$b1,$b2"
-            # if $row[$b] is undefined then it equals 0
-            row[$i,$j]=$(( ${row[$a]} + ${row[$b]:-0} ))
-            rows+=( ${row[$i,$j]} )
+            # if $row[$b] is undefined then it defaults to 0
+            rows[$i,$j]=$(( ${rows[$a]} + ${rows[$b]:-0} ))
+            cols+=( ${rows[$i,$j]} )
         done
-        echo "${space}${rows[@]}"
+        echo "${space}${cols[@]}"
         space="${space:1}"
     done
 }
