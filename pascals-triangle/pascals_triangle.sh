b9b1bfe2bb21f81e7aa1d44fff699d253ea71b70
David S. Jackson <deepbsd@yahoo.com>
2020-05-30 22:26:12 -0400

    set default on [] to zero

--- pascals-triangle/pascals_triangle.sh
+++ pascals-triangle/pascals_triangle.sh
@@ -12,6 +12,7 @@ main(){
         for ((j=1; j<=i; j++)); do
             a1=$((i-1)); a2=$((j-1)); a="$a1,$a2"
             b1=$((i-1)); b2=$j; b="$b1,$b2"
+            # if $row[$b] is undefined then it equals 0
             row[$i,$j]=$(( ${row[$a]} + ${row[$b]:-0} ))
             rows+=( ${row[$i,$j]} )
         done
