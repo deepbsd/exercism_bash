7d3c3fd9950e0844645870e49ca049a7ce89604e
David S. Jackson <deepbsd@yahoo.com>
2020-06-03 16:00:56 -0400

    transpose: trying some ideas

--- transpose/transpose.sh
+++ transpose/transpose.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
 
 main(){
-    input="$1"
-    readarray -t outarr <<<"$input"
+    readarray -t inputarr
 
-    for line in "${outarr[@]}"; do
-        echo "line: $line"
+    # get the max length of a line in input
+    for ch in "${inputarr[@]}"; do
+        (( ${#ch} >= max )) && max=${#ch}
     done
+    # iterate over and transpose each line
+    for ((i=0; i<max; i++)); do
+        unset line
+        for ch in "${inputarr[@]}"; do
+            letter=${ch:i:1}
+            line+=${letter:-+}
+        done
+        line=$(echo -e "${line}" | sed -e 's/+*$//')
+        out+="${line//+/ }"
+    done
+
+    echo -e "$out"
+    ##for line in "${outarr[@]}"; do
+    ##    echo "line: $line"
+    ##done
+    ##echo "arr: ${outarr[@]}"
+    #for ((i=0; i<"${#outarr[@]}"; i++)); do
+    #    echo "${outarr[$i]}"
+    #done
 }
 
 main "$@"
+
