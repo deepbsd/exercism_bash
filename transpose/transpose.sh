1793c3b1d311c222fc8b93b03d798d28375d172d
David S. Jackson <deepbsd@yahoo.com>
2020-06-17 23:44:08 -0400

    little more on transpose

--- transpose/transpose.sh
+++ transpose/transpose.sh
@@ -2,24 +2,23 @@
 
 main1(){
     readarray -t lines
-    declare -i count=${#lines[@]} len=${#lines[count -1]}
+    count=${#lines[@]} 
+    length=${#lines[count-1]}
 
     for (( i=count-2; i>=0; i-- )); do
         while [[ ${#lines[i]} -lt ${#lines[i+1]} ]]; do
             lines[i]="${lines[i]} "
         done
-        [[ ${#lines[i]} -gt $len ]] && len=${#lines[i]}
+        [[ ${#lines[i]} -gt $length ]] && length=${#lines[i]}
     done
 
-    for (( i=0; i<$len; ++i )); do
+    for (( i=0; i<$length; ++i )); do
         transposed=""
         for line in "${lines[@]}"; do
             transposed+=${line:i:1}
         done
-
         echo "$transposed"
     done
 }
-
 main1 "$@"
 
