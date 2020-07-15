570d06443d8df3568d83d85c2abd4608c0a978bd
David S. Jackson <deepbsd@yahoo.com>
2020-07-15 15:11:46 -0400

    still working on method2

--- pythagorean-triplet/pythagorean_triplet.sh
+++ pythagorean-triplet/pythagorean_triplet.sh
@@ -15,4 +15,41 @@ main(){
     for triplet in "${triplets[@]}"; do echo $triplet; done | sort -n
 }
 
-main "$@"
+main1(){
+    for (( a=0; a<=$1; a++ )); do
+        for (( b=a+1; b<=$1-a; b++ )); do
+            c=$(($1-a-b))
+            if (( c>b && c**2 == a**2 + b**2 )); then
+                triplets+=( "$a,$b,$c" )
+            fi
+        done
+    done
+    for triplet in "${triplets[@]}"; do echo $triplet; done | sort -n
+}
+
+main2(){
+    declare -a triplet triplets
+    declare -i a b c m n
+    sqrt=$( bc <<< "scale=0; sqrt($1)" )
+    for (( n=1; n<=sqrt; n++ )); do
+        for (( m=n+1; m<=sqrt; m++ )); do
+            a=$(( m**2 - n**2 ))
+            b=$(( 2*m*n ))
+            c=$(( m**2 + n**2 ))
+            echo "a,b,c: $a,$b,$c" | sort 
+            #if (( a+b+c == $1 )); then
+            if (( c>b && c**2 == a**2 + b**2 && a+b+c == $1 )); then
+                triplet=( $a $b $c )
+                echo "triplet: ${triplet[@]}"
+                IFS=$'\n' triplet=($(sort -n <<<"${triplet[*]}"))
+                triplets+=( "${triplet[0]},${triplet[1]},${triplet[2]}" )
+                unset IFS
+            fi
+        done
+    done
+    for triplet in "${triplets[@]}"; do echo $triplet; done | sort -n
+}
+
+
+
+main2 "$@"
